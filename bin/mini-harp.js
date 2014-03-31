ee87e78d2ef83ea899d0c49a250401271218844d
YiChenshu <ycswaves@gmail.com>
2014-03-31 21:26:51 +0800

    current-time

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -5,6 +5,13 @@ var createMiniHarp = require("index.js")
   , port = args.port || 4000
   , app = createMiniHarp();
 
-
 console.log("Starting mini-harp on http://localhost: %d",port);
-app.listen(port);
+app.use(function(request,response,next){
+  var url = request.url.split('/');
+  if(url[1] == 'current-time'){
+    response.write(new Date().toISOString());
+    response.end();
+  } else {
+    next();
+  }
+}).listen(port);
