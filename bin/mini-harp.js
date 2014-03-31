a1446e1012a287f2f7123f04377def7aa3072cdf
YiChenshu <ycswaves@gmail.com>
2014-03-31 21:01:21 +0800

    create-command

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,2 +1,10 @@
-var connect = require('connect');
-module.exports = connect;
+#!/usr/bin/env node
+var createMiniHarp = require("index.js")
+  , parseArgs = require('minimist')
+  , args = parseArgs(process.argv.slice(2))
+  , port = args.port || 4000
+  , app = createMiniHarp();
+
+
+console.log("Starting mini-harp on http://localhost: %d",port);
+app.listen(port);
