05a262e8470d131f9f4837860689dc7fa7b26799
YiChenshu <ycswaves@gmail.com>
2014-03-31 22:21:33 +0800

    minimize bin/mimi-harp.js

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -5,18 +5,8 @@ var createMiniHarp = require("mini-harp")
   , port = args.port || 4000
   , root = (typeof args._[0] === 'undefined')?
             process.cwd() : process.cwd() + '/' + args._[0]
-  , app = createMiniHarp(root)
-  , serveStatic = require('serve-static');
+  , app = createMiniHarp(root);
 
 
-console.log("Starting mini-harp on http://localhost: %d, path: %s",port,root);
-app.use(function(request, response, next){
-  var url = request.url.split('/');
-  if(url[1] == 'current-time'){
-    response.write(new Date().toISOString());
-    response.end();
-  } else {
-    next();
-  }
-}).use(serveStatic(root))
-.listen(port);
+console.log("Starting mini-harp on http://localhost: %d",port);
+app.listen(port);
--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var connect = require('connect');
-module.exports = connect;
+var connect = require('connect')
+  , serveStatic = require('serve-static');
+
+module.exports = function(path){
+  var app = connect();
+
+  app.use(function(request, response, next){
+    var url = request.url.split('/');
+    if(url[1] == 'current-time'){
+      response.write(new Date().toISOString());
+      response.end();
+    } else {
+      next();
+    }
+  }).use(serveStatic(path));
+
+  return app;
+}
