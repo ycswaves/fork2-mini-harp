cb60d04eca74294eedf00f6307170966858d2e9b
YiChenshu <ycswaves@gmail.com>
2014-03-31 22:00:25 +0800

    server-static

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
-var createMiniHarp = require("index.js")
+var createMiniHarp = require("mini-harp")
   , parseArgs = require('minimist')
   , args = parseArgs(process.argv.slice(2))
   , port = args.port || 4000
-  , app = createMiniHarp();
+  , root = (typeof args._[0] === 'undefined')?
+            process.cwd() : process.cwd() + '/' + args._[0]
+  , app = createMiniHarp(root)
+  , serveStatic = require('serve-static');
 
-console.log("Starting mini-harp on http://localhost: %d",port);
-app.use(function(request,response,next){
+
+console.log("Starting mini-harp on http://localhost: %d, path: %s",port,root);
+app.use(function(request, response, next){
   var url = request.url.split('/');
   if(url[1] == 'current-time'){
     response.write(new Date().toISOString());
@@ -14,4 +18,5 @@ app.use(function(request,response,next){
   } else {
     next();
   }
-}).listen(port);
+}).use(serveStatic(root))
+.listen(port);
