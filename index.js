73d6253ba9329f4ee51a2309d7f484c1b07b94e1
YiChenshu <ycswaves@gmail.com>
2014-04-01 23:24:55 +0800

    root-as-index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var connect = require('connect')
   , serveStatic = require('serve-static')
-  , serveJade = require('./lib/processor/jade.js');
+  , serveJade = require('./lib/processor/jade.js')
+  , serveLess = require('./lib/processor/less.js');
 
 module.exports = function(path){
   var app = connect();
@@ -10,11 +11,21 @@ module.exports = function(path){
     if(url[1] == 'current-time'){
       response.write(new Date().toISOString());
       response.end();
-    } else {
+    }
+    else if(url[1] == ''){
+      request.url = request.url+'index.html';
+      next();
+    }
+    else {
       next();
     }
   }).use(serveStatic(path))
-    .use(serveJade(path));
+    .use(serveJade(path))
+    .use(serveLess(path));
 
   return app;
+}
+
+function serveRoot(request, response, next){
+  request.url = request.url+'index.html';
 }
